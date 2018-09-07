2ea0d664d1f06155be8183eb32154dba4dc019b7
Colton Miller <colton.miller@tooeleschools.org>
2018-09-07 09:21:21 -0600

    first commit

---
+++ MadLib.py
@@ -0,0 +1,8 @@
+w6 = input("adjective")
+w7 = input("place")
+w8 = input("olor")
+w9 = input("liquid")
+w10 = input("food or something edible")
+w11 = input("food or something edible")
+print("It's been a ", w6, "winter here in ", w7, "For as long as we can Remember, the sky has been ", w8, "and the ground has been covered with", w9, "We have all been surviving on a steady diet of ", w10, "and", w11,  ".") 
+input("type to end"
---
+++ README.md
@@ -0,0 +1,1 @@
+# Cmiller2
